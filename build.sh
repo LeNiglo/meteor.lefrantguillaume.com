3d1dd1dff18e8516c94444c9a97fec5aca7598f0
Guillaume Lefrant <lefrantguillaume@gmail.com>
2018-02-14 10:41:11 +0100

    Added cute favicon

--- build.sh
+++ build.sh
@@ -16,6 +16,6 @@ cd bundle/programs/server/
 sudo npm install --production
 sudo chown $APP_NAME:leniglo -R /home/$APP_NAME
 sudo find /home/$APP_NAME -type d -exec chmod 775 {} \;
-sudo find /home/$APP_NAME -type f -exec chmod 664 {} \;
+sudo find /home/$APP_NAME ! -name '*.sh' -type f -exec chmod 664 {} \;
 sudo pm2 restart /home/leniglo/ecosystem.config.js --only $APP_NAME
 sudo pm2 show $APP_NAME
